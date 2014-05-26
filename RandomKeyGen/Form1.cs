02cc446375710ea1c4bd44d6825c15575bb8c18a
CokaCola <furbanj@gmail.com>
2014-05-26 10:51:35 +1000

    Threaded so the GUI won't freeze when generating large numbers of codes long codes. Also changed to use .net 3.5 so pretty much anyway with Windows 7 can use it without installing anything extra.

--- RandomKeyGen/Form1.cs
+++ RandomKeyGen/Form1.cs
@@ -5,7 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace RandomKeyGen
@@ -22,7 +22,6 @@ namespace RandomKeyGen
         List<char> numbers;
         List<char> lowerCase;
         List<char> upperCase;
-        List<char> symbols;
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -39,7 +38,7 @@ namespace RandomKeyGen
                 MessageBox.Show("You need to tick at least 1 checkbox.", "Error");
                 return;
             }
-            
+
             List<char> list = new List<char>();
             if (numBox.Checked)
                 list.AddRange(numbers);
@@ -48,17 +47,24 @@ namespace RandomKeyGen
             if (upperBox.Checked)
                 list.AddRange(upperCase);
 
+            PleaseWaitDialog diag = new PleaseWaitDialog();
+            
             List<string> keys = new List<string>();
             string key;
-            for (int i = 0; i < numKeys.Value; i++)
+            new Thread(new ThreadStart(delegate
             {
-                key = "";
-                for (int i1 = 0; i1 < lenBox.Value; i1++)
+                for (int i = 0; i < numKeys.Value; i++)
                 {
-                    key += list[rand.Next(0, list.Count - 1)];
+                    key = "";
+                    for (int i1 = 0; i1 < lenBox.Value; i1++)
+                    {
+                        key += list[rand.Next(0, list.Count - 1)];
+                    }
+                    keys.Add(key);
                 }
-                keys.Add(key);
-            }
+                diag.CloseDiag();
+            })).Start();
+            diag.ShowDialog();
 
             string output = "";
             for (int ii = 0; ii < keys.Count; ii++)
@@ -66,6 +72,7 @@ namespace RandomKeyGen
                 output += keys[ii] + Environment.NewLine + "-" + Environment.NewLine;
             }
             outBox.Text = output;
+
         }
     }
 }
---
+++ RandomKeyGen/PleaseWaitDialog.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RandomKeyGen
+{
+    public partial class PleaseWaitDialog : Form
+    {
+        public PleaseWaitDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void PleaseWaitDialog_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        bool canClose = false;
+        public void CloseDiag()
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new MethodInvoker(() => CloseDiag()));
+                return;
+            }
+            canClose = true;
+            this.Close();
+        }
+
+        private void PleaseWaitDialog_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!canClose)
+                e.Cancel = true;
+        }
+    }
+}
